c90137e4b5d5bb0f4392f4fb962f053e7e49f3a3
Ruben Bustos Romero <ruben.bustos@webbeds.com>
2022-09-08 18:45:26 +0200

    Add mock unit test

--- tests/routers/test_hello.py
+++ tests/routers/test_hello.py
@@ -5,3 +5,12 @@ from app.routers import hello
 async def test_hello_returns_hello_world():
     response = await hello.hello()
     assert response == "hello world"
+
+@pytest.mark.asyncio
+async def test_hello_name_returns_full_name(mocker):
+    db_user_mock = type('',(object,),{"first_name": "ruben"})()
+    mocker.patch('app.routers.hello.db.fetch_user', return_value=db_user_mock)
+
+    expected = "hello world ruben bustos"
+    actual = await hello.hello_name(last_name="bustos")
+    assert expected == actual
