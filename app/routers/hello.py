85db61910dda2c6a953ccb41de09dda7ef22370c
Ruben Bustos Romero <ruben.bustos@webbeds.com>
2022-09-08 17:40:27 +0200

    Check if user exists

--- app/routers/hello.py
+++ app/routers/hello.py
@@ -10,4 +10,7 @@ async def hello():
 @router.get("/hello/{last_name}", tags=["hello"])
 async def hello_name(last_name):
     user = await db.fetch_user(last_name)
+    if user is None:
+        return f"Who is this {last_name}??"
+    
     return f"hello world {user.first_name} {last_name}"
